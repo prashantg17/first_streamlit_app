6091b581267ae529e764fccb5b82b41c11ffd907
prashantg17 <131313988+prashantg17@users.noreply.github.com>
2023-04-19 19:15:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Health Dinner') 
+
+streamlit.header('Breakfast Manu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Indian Team and Coffee')
