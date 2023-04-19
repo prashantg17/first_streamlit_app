f6b5016fc15f750abe2561778d6e008c19a18a05
prashantg17 <131313988+prashantg17@users.noreply.github.com>
2023-04-19 19:41:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('Breakfast Favorites') 
-🥣   
+ 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
